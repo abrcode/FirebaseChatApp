bd1ca8d2d6227e2215c123d37a15edd9964c0d0b
Aniket <aniketrao1398@gmail.com>
2023-02-21 00:29:34 +0530

    Message collection View
    - Message Collection view with static data is added.

--- Messenger/Controller/ChatDetailVC.swift
+++ Messenger/Controller/ChatDetailVC.swift
@@ -20,7 +20,7 @@ struct Message : MessageType {
 
 // MARK: - Sender fields
 
-struct sender : SenderType {
+struct Sender : SenderType {
     var photoURL : String
     var senderId: String
     var displayName: String
@@ -32,11 +32,55 @@ class ChatDetailVC: MessagesViewController {
     @IBOutlet weak var msgCollectionView: MessagesCollectionView!
     
     
+    private var messages = [Message]()
+    private var selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Aniket Rao")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello world message...!")))
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "2",
+                                sentDate: Date(),
+                                kind: .text("Hello world message...! Hello world message...! Hello world message...! Hello world message...! Hello world message...! Hello world message...! Hello world message...! Hello world message...! Hello world message...!")))
+        
+        self.setUpMsgCollectionView()
         // Do any additional setup after loading the view.
     }
+    
+}
+
+// MARK: - Helper methods
+extension ChatDetailVC {
+    
+    func setUpMsgCollectionView(){
+        messagesCollectionView.messagesDisplayDelegate = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDataSource = self
+    }
+    
+}
+
+// MARK: - MessgeCllctionView Dlegate datasource
+extension ChatDetailVC : MessagesLayoutDelegate, MessagesDataSource , MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
     
 }
